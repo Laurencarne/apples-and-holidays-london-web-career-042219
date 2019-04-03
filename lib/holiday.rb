1a9fe8778787d9ffac4e34f033901e0677c909f6
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-03 12:28:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -98,7 +98,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supplies|
       if supplies.to_s.include?("BBQ")
-        return holiday
+        holiday
       end
     end
   end
