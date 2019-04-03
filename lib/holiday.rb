6311bf76d893bcb5c7efe2b9fa084d6d4a22747e
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-03 10:51:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,8 +77,11 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday, supplies|
+      if holiday.include? "_"
       holiday_array = holiday.to_s.split
       holiday = holiday_array.each {|item| item.capitalize.join(" ")}
+    else
+      holiday = holiday.to_s.capitalize
       supplies = supplies.join(", ")
       puts "  #{holiday}: #{supplies}"
 
