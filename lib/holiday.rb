495d154940ba5e806604153db9e93e36f45f941a
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-03 10:31:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,9 +78,10 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday, supplies|
       puts "#{holiday.to_s}: #{supplies.to_s}"
-      binding.pry
+
 end
 end
+binding.pry
 end
 
 def all_holidays_with_bbq(holiday_hash)
