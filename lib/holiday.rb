b5adf9520062050b677ce09d00bde4c8aa104525
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-03 10:12:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,6 @@ def all_supplies_in_holidays(holiday_hash)
   new_array = holiday_hash.keys.to_s
   new_array
 end
-end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
